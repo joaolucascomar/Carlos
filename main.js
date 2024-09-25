2f34f19cd3560e09d1c7ed9cb311f6a03eb0384f
joaolucascomar <141441384+joaolucascomar@users.noreply.github.com>
2024-09-25 08:41:01 -0300

    aaaaa

--- main.js
+++ main.js
@@ -24,11 +24,10 @@ let media = function(num1, num2, num3) {
     return (num1 + num2 + num3) / 3
 }
 
-
 num1 = parseInt(prompt("Insira o primeiro número"))
 num2 = parseInt(prompt("Insira o segundo número"))
 num3 = parseInt(prompt("Insira o terceiro número"))
-console.log(media(num1, num2, num3))
+document.write(media(num1, num2, num3))
 
 
 let mediaArray = function(array) {
