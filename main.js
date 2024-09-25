edd814df1c6f110b2788df6a0d224ac6c5ab330d
joaolucascomar <141441384+joaolucascomar@users.noreply.github.com>
2024-09-25 08:19:34 -0300

    uai

--- main.js
+++ main.js
@@ -21,5 +21,13 @@ let media = function(num1, num2, num3) {
     return (num1 + num2 + num3) / 3
 }
 
+let mediaArray = function(array) {
+    let total = 0
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];    
+    }
+    return total / array.length
+}
+
 console.log(media(4, 6, 8))
 console.log(soma(2,5));
