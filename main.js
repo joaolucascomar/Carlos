64af9f0d4495e9572e2b00f7be326e346e84af70
joaolucascomar <141441384+joaolucascomar@users.noreply.github.com>
2024-10-02 08:23:54 -0300

    asdasd

--- main.js
+++ main.js
@@ -51,9 +51,9 @@
 //     console.log(estados[i])
 // }
 
-let idade = 19;
+let idade = 16;
 
-if(idade >= 18) {
+if(!idade >= 18) {
     console.log('Pode dirigir')
 } else {
     console.log('Ilegal dirigir')
